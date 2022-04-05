072d6f226648b09594b2e445c7ff5a4cdb371cfc
dan <daniel.robert.baker1@gmail.com>
2022-04-05 23:47:39 +1200

    added favicon and some images

--- app.py
+++ app.py
@@ -1,43 +1,41 @@
-
 import dash
-from dash.dependencies import Input, Output, State
 import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 import dash_html_components as html
 import numpy as np
 import pandas as pd
-import gunicorn #whilst your local machine's webserver doesn't need this, Heroku's linux webserver (i.e. dyno) does
-from whitenoise import WhiteNoise #for serving static files on WSGI web framework (Heroku) into static folder of root
+import matplotlib as mpl
+import gunicorn                     #whilst your local machine's webserver doesn't need this, Heroku's linux webserver (i.e. dyno) does
+from whitenoise import WhiteNoise   #for serving static files on Heroku
 
-# Instantiate dash app, and underlying flask app
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.SANDSTONE] ) #
+# Instantiate dash app
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.FLATLY] ) 
 
-# Define the underlying flask app as a variable called server. (Used by gunicorn webserver in Heroku production deployment)
-server = app.server  
+# Define the underlying flask app (Used by gunicorn webserver in Heroku production deployment)
+server = app.server 
 
-# Wrap the flask app with Whitenoise allowing serving static files from static folder (which is root to heroku)
+# Enable Whitenoise for serving static files from Heroku (the /static folder is seen as root by Heroku) 
 server.wsgi_app = WhiteNoise(server.wsgi_app, root='static/') 
 
 # Define Dash layout
 def create_dash_layout(app):
 
     # Set browser tab title
-    app.title = "Browser Tab Title" #browser tab
+    app.title = "Your app title" #browser tab
     
     # Header
-    header = html.Div("Hi I am a header")
+    header = html.Div([html.Br(), dcc.Markdown(""" # Hi. I'm your Dash app."""), html.Br()])
     
     # Body (i.e. the map centrepiece, with loaders to overlay ontop)
-    body = html.Div("This is the body")
+    body = html.Div([dcc.Markdown(""" ## I'm ready to serve static files on Heroku. Just look at this! """), html.Br(), html.Img(src='charlie.png')])
 
     # Footer
-    footer = html.Div("This is the footer area")
+    footer = html.Div([html.Br(), html.Br(), dcc.Markdown(""" ### Built with ![Image](heart.png) in Python using [Dash](https://plotly.com/dash/)""")])
     
     # Assemble dash layout 
     app.layout = html.Div([header, body, footer])
 
     return app
-
 
 # Construct the dash layout
 create_dash_layout(app)
