2fbf37af025e79d22062dd30f4653d4f3389a4c9
Syed Billah <smbillah@gmail.com>
2022-04-09 08:25:04 -0400

    Update app.py

--- app.py
+++ app.py
@@ -129,5 +129,5 @@ def update_figure(selected_year):
 
 ## uncomment the following 2 lines to run remotely on heroku server
 if __name__ == "__main__":
-    app.run_server(debug=False, host='0.0.0.0', port=8050)
+    app.run_server(debug=True, host='0.0.0.0')
  
